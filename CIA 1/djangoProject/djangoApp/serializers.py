62621f5c3f1c18c7727cb09afde3a94d0bceca1a
Krish Agarwal <106873686+agarwalkrish26@users.noreply.github.com>
2023-10-12 17:13:10 +0530

    Delete CIA 1 directory

--- CIA 1/djangoProject/djangoApp/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializer
-
-from .models import APIModel
-
-class APIserializer(serializer.HyperlinkedModelSerializer):
-    
-    class Meta:
-        model=APIModel
-        fields=("title", "description")
--- CIA 1/djangoProject/djangoApp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-
-# Create your views here.
-
-def product_list(request):
-    products = Product.objects.all()[:3]  # Fetch first 3 products
-    return render(request, 'djangoApp/product_list.html', {'products': products})
